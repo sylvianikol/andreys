d232db543202bcd024f653d31ffcbcac6ca4c6e2
sylvianikol <silviaadvert@gmail.com>
2021-02-16 09:24:43 +0200

    added user register endpoint

---
+++ src/main/java/com/exam/andreys/web/UserController.java
@@ -0,0 +1,26 @@
+package com.exam.andreys.web;
+
+import com.exam.andreys.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+
+
+    public UserController(UserService userService, ModelMapper modelMapper) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+}
