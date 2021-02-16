e5c667a267b4dd7ebab6ace7313a2841b29ba263
sylvianikol <silviaadvert@gmail.com>
2021-02-16 09:06:07 +0200

    created HomeController

---
+++ src/main/java/com/exam/andreys/web/HomeController.java
@@ -0,0 +1,16 @@
+package com.exam.andreys.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(HttpSession httpSession) {
+
+        return "index";
+    }
+}
